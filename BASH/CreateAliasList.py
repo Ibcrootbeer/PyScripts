a65c2d5807b49ead2ba907217c21bd7a41a1df37
aaron middleton <aaronmiddleton@ymail.com>
2016-03-20 12:22:06 -0500

    Added tags so that I know what to do.

--- BASH/CreateAliasList.py
+++ BASH/CreateAliasList.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
+#TODO Make system give me the username
+#TODO Make system give me all users on machine and then preform this on all of them
 
 #To get the alias list run this:
-#{ ls /bin; ls /usr/bin; } | sort > /home/aaron/Desktop/PyScripts/aliases.txt
+#{ ls /bin; ls /usr/bin; } | sort > /home/aaron/Desktop/PyScripts/BASH/aliases.txt
+#TODO Run this then start script
+
 
 import fileinput
+#TODO Eventually only create one file that will execute so aliases.txt is not needed
 
-s = 'hello world'
-print s
-
-for line in fileinput.input("/home/aaron/Desktop/PyScripts/aliases.txt", inplace=True):
+for line in fileinput.input("/home/aaron/Desktop/PyScripts/BASH/aliases.txt", inplace=True):
 	output = "\tbashrc.write('alias " + line.rstrip('\n') + "=\\'exit\\'\\n')"
 	print output
 
-
-
-'''
-with open("/home/aaron/Desktop/PyScripts/aliases.txt", 'r') as inputFile, open("/home/aaron/Desktop/PyScripts/aliasestest.txt", 'w') as outputFile:
-	for line in inputFile:
-		output = "\tbashrc.write('alias " + line.rstrip('\n') + "=\\'exit\\'\\n')\n"
-		outputFile.write(output)
-'''
+#TODO Mabey make it force close any open terminals.
+#TODO End goal is that this generates the entire "Are you BASHful.py" file and ten runs it.
