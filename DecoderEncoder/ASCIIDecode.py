a57d8de6f0879d015b71f82b0e2c20be04c6c93e
aaron middleton <aaronmiddleton@ymail.com>
2016-03-23 20:39:06 -0500

    Shortened the decode method to make it make more since.

--- DecoderEncoder/ASCIIDecode.py
+++ DecoderEncoder/ASCIIDecode.py
@@ -5,14 +5,10 @@ import binascii
 userinput = raw_input()
 
 def decode(encoding):
-	tempstring = ''
-	tempstring2 = ''
+	output = ''
 	for i in range(0, len(userinput), encoding):
-		for j in range(i,i+encoding):
-			tempstring = tempstring + userinput[j]
-		tempstring2 = tempstring2 + str(unichr(int(tempstring,2)))
-		tempstring = ''
-	print tempstring2
+		output = output + str(unichr(int(userinput[i:i+encoding],2)))
+	print output
 
 if ((len(userinput) % 7 == 0) and (len(userinput) % 8 == 0)):
 	decode(7)
@@ -20,7 +16,7 @@ if ((len(userinput) % 7 == 0) and (len(userinput) % 8 == 0)):
 
 elif len(userinput) % 7 == 0:
 	decode(7)
-elif len(userinput) % 8 ==0:
+elif len(userinput) % 8 == 0:
 	decode(8)
 else:
 	print "Non-Decodable"
