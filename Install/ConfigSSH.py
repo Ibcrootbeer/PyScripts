dab3d98ee0ce24da1239cb1b07bd807d95503bad
Ibcrootbeer <aaronmiddleton@ymail.com>
2016-04-01 03:53:05 -0500

    Added config files for all the services.

---
+++ Install/ConfigSSH.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import fileinput
+import sys
+
+path = "/home/aaron/Desktop/sshd_config"
+            
+def ChangeSetting(setting, value):
+    exists = False
+    for line in fileinput.input(path, inplace=True):
+        if line.startswith(setting):
+            print setting + ' ' + value
+            exists = True
+        else:
+            print line.rstrip('\n')
+            
+    if exists == False:
+        with open(path, 'a') as sshconfig:
+            sshconfig.write('\n' + setting + ' ' + value)
+
+def RemoveSetting(setting):
+    for line in fileinput.input(path, inplace=True):
+        if line.startswith(setting):
+            print ''
+        else:
+            print line.rstrip('\n')
+            
+if len(sys.argv) > 5 or len(sys.argv) < 3:
+    print "Incorrect number of arguments " + str(len(sys.argv))
+elif len(sys.argv) == 3 and sys.argv[1] == '-p':
+    ChangeSetting("Port", sys.argv[2])
+elif len(sys.argv) == 3 and sys.argv[1] == '-a':
+    ChangeSetting("ListenAddress", sys.argv[2])
+elif len(sys.argv) == 5:
+    if sys.argv[1] == '-p':
+        ChangeSetting("Port", sys.argv[2])
+    elif sys.argv[1] == '-a':
+        ChangeSetting("ListenAddress", sys.argv[2])
+    
+    if sys.argv[3] == '-p':
+        ChangeSetting("Port", sys.argv[4])
+    elif sys.argv[3] == '-a':
+        ChangeSetting("ListenAddress", sys.argv[4])
+else:
+    print "Are the arguments in the right order?"
